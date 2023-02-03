2c7e28d471af0f377fd0efafec686e7f41bc4eac
KKakavo <pooreshqa2805@gmail.com>
2023-02-03 13:35:49 +0300

    Update MQ135.h

--- libraries/MQ135-master/MQ135.h
+++ libraries/MQ135-master/MQ135.h
@@ -21,7 +21,7 @@ v1.0 - First release
 /// The load resistance on the board
 #define RLOAD 1.0
 /// Calibration resistance at atmospheric CO2 level
-#define RZERO 54.52
+#define RZERO 80 //90
 /// Parameters for calculating ppm of CO2 from sensor resistance
 #define PARA 116.6020682
 #define PARB 2.769034857
